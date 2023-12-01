3fe402d90c102d2ed9e7515d33e3fbb64c6aadbf
isaquecsilva <isaque.costa9333@gmail.com>
2023-11-30 21:30:17 -0300

    Controller full implementation and Adapter interface update

--- src/infra/controller/Adapter.go
+++ src/infra/controller/Adapter.go
@@ -3,7 +3,7 @@ package controller
 import "github.com/isaquecsilva/posts-rest-api/core/entities"
 
 type Adapter interface {
-	GetIdParam() (int, error)
+	GetID() (int, error)
 	GetRequest() (entities.Post, error)
 	SendResponse(code int, obj any) error
 	Validate() error
--- src/infra/controller/Controller.go
+++ src/infra/controller/Controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/isaquecsilva/posts-rest-api/config/rest_errors"
@@ -9,63 +10,69 @@ import (
 
 type Controller struct {
 	postService usecase.PostServiceUseCase
-	adapter     Adapter
 }
 
-func (c *Controller) CreateNewPost() {
+func NewController(postService usecase.PostServiceUseCase) *Controller {
+	return &Controller{
+		postService,
+	}
+}
+
+func (c *Controller) CreateNewPost(adapter Adapter) {
 	// Whether users request fields are invalid
 	// Then, we return a bad request rest error message.
-	if err := c.adapter.Validate(); err != nil {
+	if err := adapter.Validate(); err != nil {
 		badRequestErrorMessage := rest_errors.NewBadRequestRestError(err.Error())
-		c.adapter.SendResponse(badRequestErrorMessage.Code, badRequestErrorMessage)
+		adapter.SendResponse(badRequestErrorMessage.Code, badRequestErrorMessage)
 		return
 	}
 
-	post, err := c.adapter.GetRequest()
+	post, err := adapter.GetRequest()
 
 	// We couldn't parse the request?
 	// So, we return an internal server rest error message.
 	if err != nil {
+		fmt.Println("chegou aqui")
 		restErrorMessage := rest_errors.NewInternalServerRestError(err.Error())
-		c.adapter.SendResponse(restErrorMessage.Code, restErrorMessage)
+		adapter.SendResponse(restErrorMessage.Code, restErrorMessage)
 		return
 	}
 
 	// If everything is ok, we call the post service
 	if err = c.postService.CreatePost(post); err == nil {
-		c.adapter.SendResponse(http.StatusOK, "Post created.")
+		adapter.SendResponse(http.StatusOK, "Post created.")
 	} else {
 		internalError := rest_errors.NewInternalServerRestError(err.Error())
-		c.adapter.SendResponse(internalError.Code, internalError)
+		adapter.SendResponse(internalError.Code, internalError)
 	}
 
 }
 
-func (c *Controller) GetPost() {
+func (c *Controller) GetPost(adapter Adapter) {
 	// Whether the postId param couldn't be
 	// get, we receive the error message and send it
 	// as a response to final user.
-	postId, err := c.adapter.GetIdParam()
+	postId, err := adapter.GetID()
 
 	if err != nil {
 		badRequest := rest_errors.NewBadRequestRestError(err.Error())
-		c.adapter.SendResponse(badRequest.Code, badRequest)
+		adapter.SendResponse(badRequest.Code, badRequest)
 		return
 	}
 
 	if post, err := c.postService.GetPost(postId); err != nil {
 		internalError := rest_errors.NewInternalServerRestError(err.Error())
-		c.adapter.SendResponse(internalError.Code, internalError)
+		adapter.SendResponse(internalError.Code, internalError)
 	} else {
-		c.adapter.SendResponse(http.StatusOK, post)
+		adapter.SendResponse(http.StatusOK, post)
 	}
 }
 
-func (c *Controller) GetAllPosts() {
+func (c *Controller) GetAllPosts(adapter Adapter) {
 	if posts, err := c.postService.GetAllPosts(); err == nil {
-		c.adapter.SendResponse(http.StatusOK, posts)
+		adapter.SendResponse(http.StatusOK, posts)
 	} else {
 		internalError := rest_errors.NewInternalServerRestError(err.Error())
-		c.adapter.SendResponse(internalError.Code, internalError)
+		adapter.SendResponse(internalError.Code, internalError)
 	}
 }
