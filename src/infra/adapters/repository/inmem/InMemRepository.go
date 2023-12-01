a1d29afbcd89de151e1d92367031b35e752da2e1
isaquecsilva <isaque.costa9333@gmail.com>
2023-11-30 21:28:09 -0300

    InMemRepo: functions logic little change

--- src/infra/adapters/repository/inmem/InMemRepository.go
+++ src/infra/adapters/repository/inmem/InMemRepository.go
@@ -1,6 +1,8 @@
 package inmem
 
 import (
+	"errors"
+
 	"github.com/isaquecsilva/posts-rest-api/core/entities"
 )
 
@@ -18,7 +20,11 @@ func (rm *RepositoryMock) Insert(post entities.Post) error {
 }
 
 func (rm *RepositoryMock) Find(id int) (entities.Post, error) {
-	return rm.DatabaseMock.Posts[id], nil
+	if len(rm.DatabaseMock.Posts) < id || id == 0 {
+		return entities.Post{}, errors.New("post not found")
+	}
+
+	return rm.DatabaseMock.Posts[id-1], nil
 }
 
 func (rm *RepositoryMock) FindAll() ([]entities.Post, error) {
