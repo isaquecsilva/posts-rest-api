19a4798e7b7ab22acd9d20b12f481b7826e8a459
isaquecsilva <isaque.costa9333@gmail.com>
2023-11-30 21:29:09 -0300

    config package: MAX_ALLOWED_REQUEST_BODY_LENGTH variable defined

--- src/config/Config.go
+++ src/config/Config.go
@@ -3,6 +3,7 @@ package config
 import "math"
 
 const (
-	MAX_NICKNAME_SIZE = 20
-	MAX_CONTENT_SIZE  = math.MaxUint8
+	MAX_NICKNAME_SIZE               = 20
+	MAX_CONTENT_SIZE                = math.MaxUint8
+	MAX_ALLOWED_REQUEST_BODY_LENGTH = 320
 )
